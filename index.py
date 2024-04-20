8458db3295ebb99d0e2391a7b6d02b08274dbec4
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-04-20 14:23:58 +0200

    dinamyc footer

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 routes = ['login', 'user']
+unique_login='blabla'
+unique_user='hable'
 
 @app.route('/')
 def home():
@@ -10,10 +12,12 @@ def home():
 @app.route('/<route>')
 def route_display(route):
     if route in routes:
-        return render_template(f'{route}.html',page=f'{route}')
+        return render_template(f'{route}.html',page=f'{route}', data = unique_login if route == 'login' else unique_user)
     else:
         # Render the index.html template dynamically
         return render_template('404.html', message='Yesss, yesss, you are in the wrong path, young hobbit.',page='not found')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
