2f6785ea125f228807c52d135a36bf7a1eb7b887
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-04-20 13:55:09 +0200

    start

---
+++ index.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+routes = ['login', 'user']
+
+@app.route('/')
+def home():
+    return render_template('index.html', message='Welcome to you first flask project',page='landing')
+
+@app.route('/<route>')
+def route_display(route):
+    if route in routes:
+        return render_template(f'{route}.html',page=f'{route}')
+    else:
+        # Render the index.html template dynamically
+        return render_template('404.html', message='Yesss, yesss, you are in the wrong path, young hobbit.',page='not found')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
